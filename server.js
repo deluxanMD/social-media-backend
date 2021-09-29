8835ec5013e29d5155d960f1ce811ceca8e3aca2
Deluxan Mariathasan <deluxan_m@outlook.com>
2021-09-29 23:06:01 +0530

    Initialized node project

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log(`Connected to Mongo DB`);
+    app.listen(8800, () => {
+      console.log(`Server is running`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Connected to Mongo DB`, err);
+  });
+
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
