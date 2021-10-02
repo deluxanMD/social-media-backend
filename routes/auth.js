f674346259b8bb3e01ca3adfd91df23f41579ef6
Deluxan Mariathasan <deluxan_m@outlook.com>
2021-10-02 16:47:59 +0530

    bcrypt for hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // REGISTER
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
 
-  const newUser = new User({ username, email, password });
-
   try {
+    // Generate salt for password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new User({ username, email, password: hashedPassword });
     const user = await newUser.save();
     res.status(201).json(user);
   } catch (error) {
     console.log(error);
+    res.status(500).send('Something went wrong');
   }
 });
 
